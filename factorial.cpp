887638c23e47a11be812787fb5951e34b35f5971
Kinglsey <kinglsey@localhost.localdomain>
2024-09-02 07:41:12 -0400

    chore: factorial

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+
+int factorial(int a) {
+    if(a <= 1)
+        return 1;
+    int factorial = 1; 
+    while(a > 1) {
+        factorial = factorial * a;
+        a = a - 1;
+    }
+    return factorial
+
+}
